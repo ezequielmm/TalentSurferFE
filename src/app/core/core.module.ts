b7bc0fd12c5663dd7330ac5be18f2e17dc539a0f
Rohit Kawade <rohit.kawade@globant.com>
2019-10-18 19:16:15 -0300

    Styles isolation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreComponentsModule  } from './_components/core-components.module';
+import { InterceptorService } from './_services/interceptor.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
@@ -10,6 +12,13 @@ import { CoreComponentsModule  } from './_components/core-components.module';
   ],
   exports:[
     CoreComponentsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    }
   ]
 })
 export class CoreModule { }
